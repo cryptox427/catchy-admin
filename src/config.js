e2d314dc44044a4d15c3ee53b272c150723b516f
Smith Zhang <smithzhang0427@gmail.com>
2022-11-07 19:05:54 -0500

    update server url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    SERVER_DOMAIN : 'https://admin.catchy.com/admin',
+    SERVER_DOMAIN : 'https://catchy.com/admin',
     ADMIN_EMAIL : 'info@cybertonic.com',
-    SERVER_HOST: 'https://admin.catchy.com'
+    SERVER_HOST: 'https://catchy.com'
 }
